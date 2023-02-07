dc1164f7f653c89be5cf2dc9d5048d6fb0e3b698
AlejandroPascualMoreno <alejandro.s262089@cesurformacion.com>
2023-02-07 16:30:34 +0100

    Primer comiit half functional

--- js/main.js
+++ js/main.js
@@ -1,26 +1,55 @@
 const api_url = "https://63dbefd4c45e08a043519fbe.mockapi.io/MenuBarPuffPuff";
-const template = document.querySelector("#platito")
-const divMostrar = document.querySelector(".menu")
-function mostrar() {
-  fetch(api_url)
-    .then(response => response.json())
-    .then(data => {
-      console.log("Mostrando platos")
-      console.table(data)
-      data.forEach(mostrarPlato)
-    })
-    .catch(err => console.log(err))
-    
-}
 
-function mostrarPlato(plato) {
-  let platito = template.content.cloneNode(true);
+fetch(api_url)
+  .then(response => response.json())
+  .then(data => mostrarData(data))
+  .catch(error => console.log(error))
+
+  const mostrarData = (data) => {
+    let body = ''
+    for(let i = 0; i<data.length; i++){
+      body += `<div class="platoGeneral"><div class="platoTexto"><h3>${data[i].nombre}</h3><p>${data[i].ingredientes}</p><p>${data[i].precio}</p><button id="favBoton">Añadir a favoritos</button></div><div class="platoImagen"><img src=${data[i].imagen}></img></div></div>`
+    }
+    document.getElementById('platitos').innerHTML = body;
+
+  }
+
+
+
+  // var bbdd = window.localStorage;
+  // var favoritos = [];
+  // var divada = document.querySelector("#platitos");
   
-  platito.querySelector(".nombre").innerText = plato.nombre;
-  platito.querySelector(".ingrediente").innerText = plato.ingredientes;//El .ingredientes se refiere al nombre de la variable del mockapi
-  platito.querySelector(".foto-plato").setAttribute("src", plato.imagen); 
-  divMostrar.appendChild(platito);
-}
-
-
-document.querySelector(".menu").mostrar;
+  // if (bbdd.getItem("favoritos")) {
+  //   favoritos = JSON.parse(bbdd.getItem("favoritos"));
+  // } else {
+  //   bbdd.setItem("favoritos", JSON.stringify(favoritos));
+  // }
+  
+  // document.getElementById("favBoton").addEventListener("click", function(e){
+  //   const botoncito = e.target.botoncito,
+  //   item = e.target,
+  //   index = favoritos.indexOf(botoncito);
+  //   bbdd.setItem("favoritos", JSON.stringify(favoritos));
+  //   mostrarFavoritos();
+  // });
+  
+  // document.querySelector("#favoritos").addEventListener("click", function () {
+  //   mostrarFavoritos();
+  // });
+  
+  // // document.querySelector("button#delete").addEventListener("click", function () {
+  // //   favoritos=[]
+  // //   bbdd.clear()
+  // //   mostrarFavoritos();
+  // // });
+  
+  // function mostrarFavoritos() {
+  //   document.querySelector("#platitos").innerText = "";
+  //   favoritos.forEach(function (el) {
+  //     console.log(el);
+  //     let n = divada.content.cloneNode(true);
+  //     n.querySelector(".platoGeneral").innerText = el;
+  //     document.querySelector("#platitos").appendChild(n);
+  //   });
+  // }
